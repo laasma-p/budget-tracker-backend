6f414e06e8f0d1b61b513ac12e4ec172275f380c
Lasma Poksane <laasmapoksane@gmail.com>
2025-09-10 19:23:27 +0200

    Add initial models and their migrations

---
+++ models/budget.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Budget extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Budget.init({
+    amount: DataTypes.DECIMAL,
+    weekStartDate: DataTypes.DATE,
+    userId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Budget',
+  });
+  return Budget;
+};
---
+++ models/transaction.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Transaction.init({
+    amount: DataTypes.DECIMAL,
+    description: DataTypes.STRING,
+    date: DataTypes.DATE,
+    userId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Transaction',
+  });
+  return Transaction;
+};
