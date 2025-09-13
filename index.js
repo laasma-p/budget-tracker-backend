7cc3964bd0fc4e35876f0a4d064cdddf55f65ade
Lasma Poksane <laasmapoksane@gmail.com>
2025-09-13 17:36:13 +0200

    Render routes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const authRoutes = require("./routes/auth-routes");
+const userRoutes = require("./routes/user-routes");
+const budgetRoutes = require("./routes/budget-routes");
+const transactionRoutes = require("./routes/transaction-routes");
+const categoryRoutes = require("./routes/category-routes");
 
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/budget", budgetRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/category", categoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
