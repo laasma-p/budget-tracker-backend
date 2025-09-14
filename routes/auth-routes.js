5aa32573ae9c9e9dcdb8b3fa0b160388ea7df64d
Lasma Poksane <laasmapoksane@gmail.com>
2025-09-14 12:27:51 +0200

    Build a skeleton for auth routes

---
+++ routes/auth-routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res, next) => {
+  // registering an user
+});
+
+router.post("/login", (req, res, next) => {
+  // logging in an user
+});
+
+module.exports = router;
